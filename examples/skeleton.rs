b793a303302bd1252fd430a8dfc514502186625c
Nash <me@aunyks.com>
2023-01-25 14:24:01 -0500

    Test this against skeletons / bone hierarchies

--- examples/skeleton.rs
+++ examples/skeleton.rs
@@ -2,16 +2,16 @@ use gltf::Gltf;
 use gltf_animation::Animation;
 
 fn main() {
-    let gltf = Gltf::open("assets/example-scene.glb").expect("Could not create glTF structure");
-    let mut animation = Animation::from_gltf(&gltf, "CubeAction")
+    let gltf = Gltf::open("assets/skeleton.glb").expect("Could not create glTF structure");
+    let mut animation = Animation::from_gltf(&gltf, "ArmatureAction.001")
         .expect("Animation doesn't exist for given target");
 
     let frames_per_second = 60.0;
     let delta_seconds = 1.0 / frames_per_second;
 
-    let simulation_duration = 4.0;
+    let simulation_duration = 1.0;
 
-    let item_name = "Cube";
+    let item_name = "bone_2";
 
     // Simulate 4 seconds
     for _ in 0..((frames_per_second * simulation_duration) as i32) {
